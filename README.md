2acee465dd8fe0a60655eb34b1e94e085fe9a31f
Axel Muñoz <axelskam.123@outlook.es>
2021-10-11 12:50:59 -0500

    Fin del segundo taller del curso

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Taller #1: Figuras Geometricas
 Calculadora de medidas de figuras geométricas básicas
+
+
+## Taller #2: Porcentajes y Descuentos
+Calculadora para hallar el porcentaje de descuento de un producto 
--- descuentos/code.js
+++ descuentos/code.js
@@ -1,4 +1,21 @@
 // Precios y descuentos
-const precio = 120;
-const porcentajeDescuento = 50;
-const precioFinal
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function buttonDiscount() {
+    const InputPrice = document.getElementById("InputPrice");
+    const precio = InputPrice.value;
+
+    const InputDiscount = document.getElementById("InputDiscount");
+    const descuento = InputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
+
+    const ResultP = document.getElementById("ResultP");
+    ResultP.innerText = `El precio con descuento es ${precioConDescuento}$`
+}
+
