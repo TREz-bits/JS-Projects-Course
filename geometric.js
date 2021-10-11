dda1135ffef93469bbf918b397828f1fb2a774f3
Axel Muñoz <axelskam.123@outlook.es>
2021-10-10 20:34:40 -0500

    Iniciando el proyecto sobre calcular figuras geometricas

---
+++ geometric.js
@@ -0,0 +1,24 @@
+// codigo del cuadrado
+
+console.group("Cuadrado");
+const ladoCuadrado = 5;
+console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log(`El perimetro del cuadrado es: ${perimetroCuadrado} cm`);
+
+const areaCuadrado = ladoCuadrado ** 2;
+console.log(`El area del cuadrado es ${areaCuadrado} cm^2`);
+
+console.groupEnd();
+
+// codigo del triangulo 
+
+console.group("Triangulo");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(`Los lados del triangulo miden: ${ladoTriangulo1} cm, ${ladoTriangulo2} cm y ${baseTriangulo} cm`);
+
+console.groupEnd();
