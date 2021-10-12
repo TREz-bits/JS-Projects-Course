e5c4c952f14fd059748a190d845d581293c087cf
Axel Muñoz <axelskam.123@outlook.es>
2021-10-12 14:00:27 -0500

    añadi cupones para descuentos para el taller #2

--- descuentos/code.js
+++ descuentos/code.js
@@ -10,12 +10,38 @@ function buttonDiscount() {
     const InputPrice = document.getElementById("InputPrice");
     const precio = InputPrice.value;
 
+    const coupons = ["AXEL", "DISCOUNT_PLATZI", "PREMIO2021", ""];
+    const InputCoupon = document.getElementById("InputCoupon");
+    const coupon = InputCoupon.value;
+    const discountCoupon = couponPriceDiscount(coupon, coupons);
+    
     const InputDiscount = document.getElementById("InputDiscount");
-    const descuento = InputDiscount.value;
+    const descuento = parseInt(InputDiscount.value) + discountCoupon;
+    console.log(descuento)
 
     const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
 
     const ResultP = document.getElementById("ResultP");
     ResultP.innerText = `El precio con descuento es ${precioConDescuento}$`
 }
+
+// cupones de descuento
+
+
+function couponPriceDiscount (coupon, coupons) {
+
+    if (!coupons.includes(coupon)) {
+        alert("El cupón " + coupon + " no es válido");
+     } else if (coupon === "AXEL") {
+        return 15;
+     } else if (coupon === "DISCOUNT_PLATZI") {
+        return 30;
+     } else if (coupon === "PREMIO2021") {
+        return 25;
+     } else if (coupon === "") {
+        return 0;
+     }
+}
+
+
 
